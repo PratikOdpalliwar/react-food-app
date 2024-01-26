e476aed5cfdb0aba0dac543c4e3670fb717eda6e
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-01-26 21:40:41 +0530

    design of FoodBox is changed

--- src/components/FoodBox/FoodBox.jsx
+++ src/components/FoodBox/FoodBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FoodBox = () => {
   return (
-    <div className="m-5 border-2 border-black rounded h-36 w-72">
+    <div className="m-5 border-2 border-black rounded h-36 w-72 ">
     
     <div className="flex flex-row">
         <img 
@@ -10,16 +10,18 @@ const FoodBox = () => {
         src="./background/food.jpg" />
 
         <div className="h-32 w-full flex justify-between flex-col">
-            <h1 className="font-extrabold text-lg my-2">
+            <h1 className="font-extrabold text-lg my-2 flex justify-center">
                 Boiled Egg
             </h1>
-            <p className="text-sm font-medium">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            </p>
-            <div className="flex justify-end">
-            <button className="w-14 h-4 m-2 p-3 bg-red-500 text-white font-semibold flex justify-center items-center font-sans rounded text-sm">
-            All
+            <div className="flex flex-col">
+            <button className="w-11/12 h-8 my-2 p-3 text-green-500 border-2 border-black font-semibold flex justify-center items-center font-sans rounded text-xs">
+            Ingredients
         </button>
+
+            <button className="w-11/12 h-8 my-2 p-3 text-red-500 border-2 border-black font-semibold flex justify-center items-center font-sans rounded text-xs">
+                See Complete Recipe
+            </button>
+          
             </div>
            
         </div>
